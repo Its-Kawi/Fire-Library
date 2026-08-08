05c3c5ef07a073a740c352698db4f84dc27c1f23
Kawi <sk4ndi1488@gmail.com>
2026-08-09 00:37:28 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -40,7 +40,7 @@ local function httpGet(url)
 end
 
 local function getLastCommitID()
-	return http:JSONDecode(httpGet("https://api.github.com/repos/Null-Cherry/Fire-Library/commits?path=Files%2FLibrary%2Elua", true))[1].sha
+	return http:JSONDecode(httpGet("https://api.github.com/repos/Its-Kawi/Fire-Library/commits?path=Files%2FLibrary%2Elua", true))[1].sha
 end
 
 local function fix(str)
@@ -77,7 +77,7 @@ if success then
 end
 
 local function update()
-	local contents = httpGet("https://raw.githubusercontent.com/Null-Cherry/Fire-Library/refs/heads/main/Files/Library.lua", true)
+	local contents = httpGet("https://raw.githubusercontent.com/Its-Kawi/Fire-Library/refs/heads/main/Files/Library.lua", true)
 	local local1 = contents:find("local", 1, true)
 	if not local1 then return false, "Invalid request return" end
 
