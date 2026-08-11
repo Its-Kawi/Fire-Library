6679e64e72afbb17bf6fa1152228d75ebb3bed70
Kawi <sk4ndi1488@gmail.com>
2026-08-11 03:11:55 +0300

    Update QuickLoader.lua

--- QuickLoader.lua
+++ QuickLoader.lua
@@ -6,6 +6,7 @@
 
 ---
 
+local GITHUB =  "https://api.github.com/repos/Null-Cherry/"
 local http = game:GetService("HttpService")
 local sg = game:GetService("StarterGui")
 
@@ -32,7 +33,7 @@ local function httpGet(url)
 end
 
 local function getLastCommitID()
-	return http:JSONDecode(httpGet("https://api.github.com/repos/Its-Kawi/Fire-Library/commits?path=Files%2FLibrary%2Elua", true))[1].sha
+	return http:JSONDecode(httpGet(GITHUB .. "/Fire-Library/commits?path=Files%2FLibrary%2Elua", true))[1].sha
 end
 
 local function fix(str)
@@ -66,7 +67,7 @@ local success, error
 local spawn = task.spawn
 
 local function update()
-	local contents = httpGet("https://raw.githubusercontent.com/Its-Kawi/Fire-Library/refs/heads/main/Files/Library.lua", true)
+	local contents = httpGet(GITHUB .. "Fire-Library/refs/heads/main/Files/Library.lua", true)
 	local local1 = contents:find("local", 1, true)
 	if not local1 then return false, "Invalid request return" end
 
